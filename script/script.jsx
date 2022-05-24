ee4e06fb21a6fd6ba05af1185567ee133814a7b6
yutosuda <yuto.suda1024@gmail.com>
2022-05-24 11:34:38 +0900

    フォルダ整理

---
+++ script/script.jsx
@@ -0,0 +1,12 @@
+const app = document.getElementById("app");
+
+function HomePage() {
+  return (
+    <div>
+      {/* <Header title="React " /> */}
+      <p>hoge</p>
+    </div>
+  );
+}
+
+ReactDOM.render(<HomePage />, app);
